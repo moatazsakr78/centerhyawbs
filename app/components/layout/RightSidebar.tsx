156ea16a9f0263102f173691dcd5c80d48ddb393
CDS Alex <cds.MoatazEhab98262@alexu.edu.eg>
2025-11-23 00:41:22 +0200

    fix the eye button

--- app/components/layout/RightSidebar.tsx
+++ app/components/layout/RightSidebar.tsx
@@ -64,16 +64,16 @@ export default function RightSidebar({ isOpen, onClose }: RightSidebarProps) {
     <>
       {/* Backdrop */}
       {isOpen && (
-        <div 
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity"
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-[60] transition-opacity"
           onClick={onClose}
         />
       )}
-      
+
       {/* Sidebar */}
-      <div 
+      <div
         ref={sidebarRef}
-        className={`fixed top-20 right-0 h-[calc(100vh-80px)] w-96 bg-[#eaeaea] border-l border-gray-400 shadow-2xl z-50 transform transition-transform duration-300 ease-in-out ${
+        className={`fixed top-20 right-0 h-[calc(100vh-80px)] w-96 bg-[#eaeaea] border-l border-gray-400 shadow-2xl z-[70] transform transition-transform duration-300 ease-in-out ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
